721bda9889841f92ce32a00ffb9365eea0e4ba84
JV08 <josephd.vargaso@konradlorenz.edu.co>
2024-03-11 19:23:22 -0500

    Edad
    Se resolvió el ejercicio de edad

--- src/edad.py
+++ src/edad.py
@@ -1,6 +1,21 @@
 def evaluar(dia, mes, anno):
-    # TODO: Coloca aquí el código del ejercicio 6: Edad
-    return "";
+   if dia <= 0 or mes <= 0:
+       return'Ingrese datos coherentes'
+   else:
+        
+    from time import localtime
+
+    t = localtime()
+    diat = t.tm_mday       
+    mest = t.tm_mon       
+    annot = t.tm_year           
+    annos = annot-anno-((mest,diat)<(mes,dia))
+    
+    if dia <= 31 and dia >= 1 and mes <= 12 and mes >= 1 and (diat, mest)>= (dia, mes) and annos > 0:
+        return "Usted tiene {} años".format(annos)         
+    else:    
+     return 'Ingrese datos coherentes'
+    
 
 if __name__ == '__main__':
     print("Ingrese su fecha de nacimiento")
@@ -13,3 +28,4 @@ if __name__ == '__main__':
         
     respuesta = evaluar(dia, mes, anno)
     print(respuesta)
+
--- tests/test.edad.py
+++ tests/test.edad.py
@@ -1,9 +1,6 @@
 import sys
 from pathlib import Path
 import unittest
-
-# Se adiciona el path del directorio padre,
-# para que podamos ejecutar los tests sin inconveniente
 root_path = Path(__file__).resolve().parent.parent
 print('root_path:')
 print(root_path)
@@ -13,11 +10,20 @@ from src.edad import evaluar
 
 class TestEdad(unittest.TestCase):
     def test2000Enero1(self):
-        valor_esperado = "Usted tiene 24 años"
-        valor_actual = evaluar(1, 1, 2024)
+        valor_esperado = 'Usted tiene 24 años'
+        valor_actual = evaluar(1, 1, 2000)
         self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
+    def test2020Marzo9(self):
+        valor_esperado = 'Usted tiene 4 años'
+        valor_actual = evaluar(9, 3, 2020)
+        self.assertEqual(valor_esperado, valor_actual)
+        
+    def test1948Junio14(self):
+        valor_esperado = 'Usted tiene 74 años'
+        valor_actual = evaluar(14, 6, 1948)
+        self.assertEqual(valor_esperado, valor_actual)
+    
     
 
 if __name__ == '__main__':
