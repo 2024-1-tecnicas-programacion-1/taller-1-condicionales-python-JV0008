622c2c212eb7b7e648c10ddbf1544455cd2476c3
JV08 <josephd.vargaso@konradlorenz.edu.co>
2024-03-11 19:21:36 -0500

    Triangulo
    Se resolvió el ejercicio del triangulo

--- src/triangulo.py
+++ src/triangulo.py
@@ -1,6 +1,14 @@
 def evaluar(a, b, c):
-    # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
+    if a <0 or b < 0 or c < 0 or a + b < c or c + b < a or c + a < b:
+        return "No es un triángulo válido"
+    elif a == b and b == c and a == c:
+        return "El triángulo es equilátero"
+    elif a != b and a != c and b != c:
+        return "El triángulo es escaleno"
+    elif a == b or b == c or c == a:
+        return "El triángulo es isóceles"     
+    
+    return "";
 
 if __name__ == '__main__':
     print("a:", end="")
--- tests/test.triangulo.py
+++ tests/test.triangulo.py
@@ -1,9 +1,6 @@
 import sys
 from pathlib import Path
 import unittest
-
-# Se adiciona el path del directorio padre,
-# para que podamos ejecutar los tests sin inconveniente
 root_path = Path(__file__).resolve().parent.parent
 print('root_path:')
 print(root_path)
@@ -16,8 +13,24 @@ class TestTriangulo(unittest.TestCase):
         valor_esperado = "No es un triángulo válido"
         valor_actual = evaluar(3.9, 6.0, 1.2)
         self.assertEqual(valor_esperado, valor_actual)
+        
+
+    def El_triángulo_es_equilátero(self):
+        valor_esperado = "El triángulo es equilátero"
+        valor_actual = evaluar(1.2, 1.2, 1.2)
+        self.assertEqual(valor_esperado, valor_actual)
+        
+
+    def El_triángulo_es_escaleno(self):
+        valor_esperado = "El triángulo es escaleno"
+        valor_actual = evaluar(3.9, 5.0, 4.2)
+        self.assertEqual(valor_esperado, valor_actual)
+        
+    def El_triángulo_es_escaleno(self):
+        valor_esperado = "El triángulo es isóceles"
+        valor_actual = evaluar(4.2, 5.6, 4.2)
+        self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
     
 
 if __name__ == '__main__':
