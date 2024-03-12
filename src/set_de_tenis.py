0f2fb73b97d63c75c94197262aa9da25e3bf0ee2
JV08 <josephd.vargaso@konradlorenz.edu.co>
2024-03-11 19:22:26 -0500

    Set de tenis
    Se resolvió el ejercicio del set de tenis

--- src/set_de_tenis.py
+++ src/set_de_tenis.py
@@ -1,6 +1,19 @@
 def evaluar(num_victorias_a, num_victorias_b):
-    # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
+    if num_victorias_a < 0 or num_victorias_b < 0 or num_victorias_b > 7 or num_victorias_a > 7 or num_victorias_b == num_victorias_a == 7 or (num_victorias_a == 7 and num_victorias_b <= 4) or (num_victorias_b == 7 and num_victorias_a <= 4):
+        return'Inválido'
+    elif num_victorias_a == 7 and num_victorias_b == 6:        
+        return'Ganó A'
+    elif num_victorias_b == 7 and num_victorias_a == 6:
+        return'Ganó B'
+    elif num_victorias_b <= 7 and num_victorias_a <= 7 and num_victorias_a - num_victorias_b >= 2 and num_victorias_a >= 6:
+        return 'Ganó A'
+    elif num_victorias_b <= 7 and num_victorias_a <= 7 and num_victorias_b - num_victorias_a >= 2 and num_victorias_b >= 6:
+        return'Ganó B'
+    elif num_victorias_a - num_victorias_b == 1 or num_victorias_b - num_victorias_a == 1 or num_victorias_a - num_victorias_b == 0 and num_victorias_b <= 7 and num_victorias_a <=7 and num_victorias_b != 7 and num_victorias_a != 7 or (num_victorias_a < 6) - num_victorias_b <= 2 or (num_victorias_b < 6) - num_victorias_a <=2:
+        return'Aún no termina'  
+    
+        
+    return "";
 
 if __name__ == '__main__':
     print("Los juegos ganaddor por A:", end="")
--- tests/test.set_de_tenis.py
+++ tests/test.set_de_tenis.py
@@ -1,9 +1,6 @@
 import sys
 from pathlib import Path
 import unittest
-
-# Se adiciona el path del directorio padre,
-# para que podamos ejecutar los tests sin inconveniente
 root_path = Path(__file__).resolve().parent.parent
 print('root_path:')
 print(root_path)
@@ -16,9 +13,44 @@ class TestSetDeTenis(unittest.TestCase):
         valor_esperado = "Aún no termina"
         valor_actual = evaluar(4, 5)
         self.assertEqual(valor_esperado, valor_actual)
+        
+    def test2(self):
+        valor_esperado = "Inválido"
+        valor_actual = evaluar(3, 7)
+        self.assertEqual(valor_esperado, valor_actual)
+        
+    def test3(self):
+        valor_esperado = "Ganó A"
+        valor_actual = evaluar(7, 6)
+        self.assertEqual(valor_esperado, valor_actual)
+        
+    def test4(self):
+        valor_esperado = "Aún no termina"
+        valor_actual = evaluar(5, 6)
+        self.assertEqual(valor_esperado, valor_actual)
+        
+    def test5(self):
+        valor_esperado = "Ganó B"
+        valor_actual = evaluar(5, 7)
+        self.assertEqual(valor_esperado, valor_actual)
+        
+    def test6(self):
+        valor_esperado = "Inválido"
+        valor_actual = evaluar(8, 6)
+        self.assertEqual(valor_esperado, valor_actual)
+        
+    def test7(self):
+        valor_esperado = "Ganó A"
+        valor_actual = evaluar(6, 4)
+        self.assertEqual(valor_esperado, valor_actual)
+        
+    def test8(self):
+        valor_esperado = "Aún no termina"
+        valor_actual = evaluar(2, 5)
+        self.assertEqual(valor_esperado, valor_actual)
     
-    # TODO: Agrega tus otros casos de prueba aquí
     
 
 if __name__ == '__main__':
     unittest.main()
+
